72262d0b8e4d6c9ab9df98df86ae7eba50b104dc
HaiyingWang22 <horizont072@gmail.com>
2024-09-14 23:58:08 +0200

    1

--- tests/web/test_base.py
+++ tests/web/test_base.py
@@ -9,16 +9,24 @@ class WebBase:
         """ Setup to run once
             Initiatiung some common parameters
         """
-        cls.app_url = 'http://localhost:8080'
+        # cls.app_url = 'http://localhost:8080'
+        cls.app_url = 'http://host.docker.internal:8080'
+        
 
     def setup_method(self):
         """ Setup to run before every test
             Initiate a new driver.
         """
+        # chrome_service = ChromeService("/path/to/chromedriver")
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument("--disable-search-engine-choice-screen")
-        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options = chrome_options)
-        #self.driver = webdriver.Remote(command_executor = "http://localhost:4444", options=chrome_options)
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--headless") 
+        # self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options = chrome_options)
+        self.driver = webdriver.Remote(command_executor = "http://localhost:4444", options=chrome_options)
+        self.driver.implicitly_wait(10)
         self.driver.set_window_size(1920,1080)
         self.driver.get(self.app_url)
 
