7d1e0389f8d9eb95e981dd118528ae13e0ff750f
HaiyingWang22 <horizont072@gmail.com>
2024-09-16 11:28:29 +0200

    old version

--- tests/web/web_test.py
+++ tests/web/web_test.py
@@ -7,58 +7,68 @@ from tests.web.pages.calculator_page import CalculatorPage
 import time
 
 class TestWeb(WebBase):
-    def setup_method(self, method):
-        self.userName = 'testUser'
-        self.password = 'test12345'
-        self.loginPage = LoginPage(self.driver)
-        self.registerPage = RegisterPage(self.driver)
-        self.calculator_page = CalculatorPage(self.driver)
-
     def test_login(self):
         LoginPage(self.driver).login('admin','test1234')
-        assert self.calculator_page.elements.username.text == 'admin'
-        self.calculator_page.elements.logout.click()   
+        assert CalculatorPage(self.driver).elements.username.text == 'admin'
+        CalculatorPage(self.driver).elements.logout.click()   
 
     def test_register_newuser(self):
+        userName='testUser'
+        password='test12345'
+        loginPage=LoginPage(self.driver)
+        registerPage=RegisterPage(self.driver)
+        calculator_page = CalculatorPage(self.driver)
         
         # register new user
-        self.loginPage.elements.register_btn.click()   
-        self.registerPage.register(self.userName,self.password) 
+        loginPage.elements.register_btn.click()   
+        registerPage.register(userName,password) 
         current_url = self.driver.current_url
         print(f"current URL is:{current_url}")
         if current_url=='http://host.docker.internal:8080/index.html':
             time.sleep(10)
-            assert self.calculator_page.elements.username.text == self.userName
-            self.calculator_page.elements.logout.click()  
+            assert calculator_page.elements.username.text == userName
+            calculator_page.elements.logout.click()  
 
         # login the user 
-        self.loginPage.login(self.userName,self.password)
-        assert self.calculator_page.elements.username.text == self.userName
+        LoginPage(self.driver).login(userName,password)
+        assert calculator_page.elements.username.text == userName
+
+        
+    #     loginPage.elements.register_btn.click()   
+    #     registerPage.register(userName,password) 
+    #     if registerPage.element.message.text == 'User already exists!': 
+    #         # self.driver.get("http://localhost:8080/login.html")
+    #         self.driver.get("http://host.docker.internal:8080/login.html") 
+    #         LoginPage(self.driver).login(userName,password)
+    #         assert calculator_page.elements.username.text == userName 
+    #     else:
+    #         time.sleep(5)
+    #         assert calculator_page.elements.username.text == userName
 
         # Perform calculations
-        self.calculator_page.calculator('add', 1, 2)  
-        self.calculator_page.calculator('subtract', 3, 2)  
-        self.calculator_page.calculator('multiply', 2, 2)  
-        self.calculator_page.calculator('divide', 4, 0)  
+        calculator_page.calculator('add', 1, 2)  
+        calculator_page.calculator('subtract', 3, 2)  
+        calculator_page.calculator('multiply', 2, 2)  
+        calculator_page.calculator('divide', 4, 0)  
         
         # Check history 
-        self.calculator_page.elements.history_btn.click()
-        expressions = self.calculator_page.element.history.value.split("\n")
+        calculator_page.elements.history_btn.click()
+        expressions = calculator_page.element.history.value.split("\n")
         print(f"history text: {expressions}") 
         for expression in expressions:
             if "=" in expression:
                 formula, result = expression.split("=")
                 if "+" in formula:
                     P1, P2 = formula.split("+")
-                    self.calculator_page.calculator('add', P1, P2)  
+                    calculator_page.calculator('add', P1, P2)  
                 if "-" in formula:
                     P1, P2 = formula.split("-")
-                    self.calculator_page.calculator('subtract', P1, P2)     
+                    calculator_page.calculator('subtract', P1, P2)     
                 if "*" in formula:
                     P1, P2 = formula.split("*")
-                    self.calculator_page.calculator('multiply', P1, P2)  
+                    calculator_page.calculator('multiply', P1, P2)  
                 if "/" in formula:
                     P1, P2 = formula.split("/")
-                    self.calculator_page.calculator('divide', P1, P2)   
+                    calculator_page.calculator('divide', P1, P2)   
 
-        self.calculator_page.elements.logout.click()  
+        calculator_page.elements.logout.click()  
